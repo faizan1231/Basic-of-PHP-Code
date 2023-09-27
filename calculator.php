86637e2d2086559e8e30044e780ee0204e6b641a
faizan1231 <139433319+faizan1231@users.noreply.github.com>
2023-09-27 23:26:40 +0500

    Add files via upload

---
+++ calculator.php
@@ -0,0 +1,21 @@
+<?php
+
+ $x=5;
+ $y=10;
+ $s ="*";
+
+ if($s == "+")
+ echo $x+$y;
+ 
+ else if($s == "-")
+ echo $x-$y;
+
+ else if($s == "*")
+ echo $x*$y;
+
+ else if($s == "/")
+ echo $x/$y;
+ else
+ echo "invalid";
+
+ ?>
