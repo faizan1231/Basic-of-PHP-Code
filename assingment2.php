6e36c3f7d646bf437641b0bb7b09da58770d325c
faizan1231 <139433319+faizan1231@users.noreply.github.com>
2023-09-30 14:25:27 +0500

    Add files via upload

---
+++ assingment2.php
@@ -0,0 +1,40 @@
+
+<form name="calculator" action="" method="GET">
+    
+     <b>First Number:</b>   <input type="text" name="firstnum" placeholder="Enter first number" required><br>
+    <b>Second Number:</b>  <input type="text" name="secondnum"  placeholder="Enter second number" required><br>
+           <b>Operator:</b> <input type="text" name="operator" size="1"  required><br>
+           <input type="submit" name="cal" value="Calculate" style="height :25px;width: 100px;"><br>
+      <br><b>Result is:<b>
+</form>
+
+<?php
+if(isset($_GET['cal'])){
+    $result ='' ;
+$fnum = $_GET['firstnum'];
+$snum = $_GET['secondnum'];
+$opr= $_GET['operator'];
+if($opr == "+")
+
+  echo $fnum+$snum;
+
+elseif($opr == "-")
+
+  echo $fnum-$snum;
+
+elseif($opr == "*")
+
+    echo $fnum*$snum;
+
+elseif($opr == "/")
+
+       echo $fnum/$snum;
+
+elseif($opr == "%")
+
+    echo $fnum%$snum;
+
+else
+   echo "invalid operator";
+}
+  ?>
